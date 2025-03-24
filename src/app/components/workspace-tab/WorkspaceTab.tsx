206e550b9e1680cf641f8658e49c1afc8ed19ef7
24HP2962 24HP2962 <24HP2962@admins-iMac.local>
2025-03-24 16:45:29 +0800

    feat: sidebar group

--- src/app/components/workspace-tab/WorkspaceTab.tsx
+++ src/app/components/workspace-tab/WorkspaceTab.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { Button } from "@/components/ui/button";
-// import { useState } from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -26,7 +25,6 @@ type WorkspaceTab = {
 };
 
 export const WorkspaceTab = ({ setShowWorkSpace }: WorkspaceTab) => {
-  // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -34,121 +32,56 @@ export const WorkspaceTab = ({ setShowWorkSpace }: WorkspaceTab) => {
     },
   });
 
-  // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
     console.log(values);
   }
   return (
-    // <div>
-    //   {!showWorkSpace && (
-    //     <div className=" text-[#777777] pb-2 px-1 flex justify-between items-center text-sm font-semibold ">
-    //       Workspaces
-    //       <Button
-    //         onClick={() => setShowWorkSpace(true)}
-    //         className="bg-inherit shadow-none hover:bg-[#E8E8E8] "
-    //       >
-    //         <Plus className="text-[#777777]" />
-    //       </Button>
-    //     </div>
-    //   )}
-    //   {showWorkSpace && (
-    //     <div>
-    //       <div className=" text-[#777777] pb-2 px-1 flex justify-between items-center text-sm font-semibold ">
-    //         Workspaces
-    //         <Button
-    //           onClick={() => setShowWorkSpace(false)}
-    //           className="bg-inherit shadow-none hover:bg-[#E8E8E8] "
-    //         >
-    //           <Plus className="text-[#777777]" />
-    //         </Button>
-    //       </div>
-    //       <div className="w-[640px] p-[30px] z-30 absolute left-[600px] bg-white rounded-xl ">
-    //         <h1 className="text-[#1d1d1d] font-inter text-xl font-bold mb-6 flex items-center justify-between">
-    //           New Workspace
-    // <Button
-    //   className="bg-inherit hover:bg-[#dcdcdc] p-2 shadow-none text-[#5e5e5e] "
-    //   onClick={() => setShowWorkSpace(false)}
-    // >
-    //   <X size={30} />
-    // </Button>
-    //         </h1>
-    //         <form>
-    //           <p className="font-inter text-[#616161] text-base font-semibold mb-6 ">
-    //             Enter the name of your new Workspace
-    //           </p>
-    //           <label>
-    //             <input
-    //               type="text"
-    //               placeholder="Enter name here"
-    //               className="w-[578px] placeholder:text-[#e5e7eb] border border-[#e5e7eb]  text-base font-inter font-semibold py-3 px-2 rounded-xl mb-8"
-    //             />
-    //           </label>
-    //           <div className="flex justify-between ">
-    //             <Button
-    //               className="w-[283px] bg-inherit hover:bg-[#dcdcdc] text-[#1d1d1d] border border-[#1d1d1d] py-3 px-4 shadow-none text-base font-inter font-semibold "
-    //               onClick={() => setShowWorkSpace(false)}
-    //             >
-    //               Cancel
-    //             </Button>
-    //             <Button className="w-[283px] bg-[#6f47eb] hover:bg-[#5f3cca] text-white py-3 px-4 shadow-none text-base font-inter font-semibold ">
-    //               Create
-    //             </Button>
-    //           </div>
-    //         </form>
-    //       </div>
-    //     </div>
-    //   )}
-    // </div>
-    <div>
-      <div className="fixed bg-[#c0c6c86a] z-30 inset-0 flex items-center justify-center">
-        <div className="w-[640px] p-[30px] z-30 absolute left-[600px] bg-white rounded-xl ">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <div className="flex justify-between">
-                      <FormLabel className="text-[#1d1d1d] font-inter text-xl font-bold pb-6 flex items-center justify-between">
-                        New Workspace
-                      </FormLabel>
-                      <Button
-                        className="bg-inherit hover:bg-[#dcdcdc] p-2 shadow-none text-[#5e5e5e] "
-                        onClick={() => setShowWorkSpace(false)}
-                      >
-                        <X size={30} />
-                      </Button>
-                    </div>
-                    <FormDescription className="font-inter text-[#616161] text-base font-semibold pb-6">
-                      Enter the name of your new Workspace
-                    </FormDescription>
-                    <FormControl>
-                      <Input placeholder="Enter name here" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <div className="flex justify-between">
-                <Button
-                  className="w-[283px] bg-inherit hover:bg-[#dcdcdc] text-[#1d1d1d] border border-[#1d1d1d] py-3 px-4 shadow-none text-base font-inter font-semibold "
-                  onClick={() => setShowWorkSpace(false)}
-                >
-                  Cancel
-                </Button>
-                <Button
-                  type="submit"
-                  className="w-[283px] bg-[#6f47eb] hover:bg-[#5f3cca] text-white py-3 px-4 shadow-none text-base font-inter font-semibold "
-                >
-                  Create
-                </Button>
-              </div>
-            </form>
-          </Form>
-        </div>
+    <div className="fixed bg-[#c0c6c86a] z-30 inset-0 flex items-center justify-center">
+      <div className="w-[640px] p-[30px] z-30 absolute left-[600px] bg-white rounded-xl ">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="flex justify-between">
+                    <FormLabel className="text-[#1d1d1d] font-inter text-xl font-bold pb-6 flex items-center justify-between">
+                      New Workspace
+                    </FormLabel>
+                    <Button
+                      className="bg-inherit hover:bg-[#dcdcdc] p-2 shadow-none text-[#5e5e5e] "
+                      onClick={() => setShowWorkSpace(false)}
+                    >
+                      <X size={30} />
+                    </Button>
+                  </div>
+                  <FormDescription className="font-inter text-[#616161] text-base font-semibold pb-6">
+                    Enter the name of your new Workspace
+                  </FormDescription>
+                  <FormControl>
+                    <Input placeholder="Enter name here" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex justify-between">
+              <Button
+                className="w-[283px] bg-inherit hover:bg-[#dcdcdc] text-[#1d1d1d] border border-[#1d1d1d] py-3 px-4 shadow-none text-base font-inter font-semibold "
+                onClick={() => setShowWorkSpace(false)}
+              >
+                Cancel
+              </Button>
+              <Button
+                type="submit"
+                className="w-[283px] bg-[#6f47eb] hover:bg-[#5f3cca] text-white py-3 px-4 shadow-none text-base font-inter font-semibold "
+              >
+                Create
+              </Button>
+            </div>
+          </form>
+        </Form>
       </div>
     </div>
   );
